0039783ecb0048b1fa1188665be450f15776a38c
ananthkrishna96 <ananthakrishnanom@gmail.com>
2020-10-30 16:26:24 +0100

    Coding Phase 2

--- pop.py
+++ pop.py
@@ -10,15 +10,17 @@ import random as rn
 
 
 def atm(bal,pn):
-    restart=('Y')
-    chance=3
+    restart=['Y','y','Yes','yes']
+    temp=restart
+    mon=[50,100,200,500,2000]
+    chance=2
     balance=bal
-    yield balance
+   
     
     while chance>=0:
-        pin=int(input("Please Enter your PIN code: \n"))
+        pin=int(input("\n Please Enter your PIN code: "))
         if pin == pn:
-            print("You pin looks cool ! \n")
+            print("\n You pin looks cool ! \n")
             while restart not in ('n', 'N', 'NO', 'no'):
                 print('Press 1 for BALANCE \n')
                 print('Press 2 for WITHDRAWAL\n')
@@ -28,12 +30,66 @@ def atm(bal,pn):
                 
                 if opt ==1:
                     print("\n Your BALANCE is: ", bal)
+                    x=input(" \n Go Back ?: ")
+                    if not x in restart:
+                        print(" Thank You !")
+                        break
+                elif opt == 2:
+                    op2=['Y','Yes','yes']
+                    print(" \n \n Please Choose Denominations of 50, 100, 200, 500, 2000")
+                    withdraw=float(input(" \n Enter the amount to withdraw: "))
+                    if withdraw in mon:
+                        balance=balance-withdraw
+                        print(" \n Your New  BALANCE is: ", balance)
+                        x=input(" \n Go Back ?: ")
+                        if not x in restart:
+                            print(" \n Thank You !")
+                            break
+                    elif withdraw not in mon:
+                        print("\n Invalid amount !")
+                        restart=temp
+                elif opt ==3:
+                    py=float(input(("\n  How Much to PAY: ")))
+                    balance+=py
+                    print(" \n Your BALANCE is: ", balance)
+                    x=input(" \n Go Back ?: ")
+                    if not x in restart:
+                        print(" \n Thank You !")
+                        break
                     
+                elif opt==4:
+                    print("\n Your Card is Removed \n Thank You !")
+                    break
+                else:
+                    print("\n Choose Correct Option  !")
+                    restart=temp
+        elif pin != pn:
+            print("\n Incorrect Password !")
+            chance-=1
+            if chance==0:
+                print("\n No more tries  !")
+                break
     
-    
-
-
 bal=int(rn.randrange(5000,100000))
+   
+acc = {
+       1 : {
+               "name" : "Ananth",
+               "pin" : 1234
+           },
+       2 : {
+               "name" : "Tobias",
+               "pin" : 1111
+           },
+       3:  {
+               "name" : "Linus",
+               "pin" : 2222
+           }
+      }
+
+
 pn=1234
-print(" Welcome to POPCORN ATM")
+    
+print(" \n \n Welcome to POPCORN ATM")
+atm(bal,pn)
 
