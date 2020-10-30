63c9727995e396f99c332db98fc6a4e9e91b62d9
ananthkrishna96 <ananthakrishnanom@gmail.com>
2020-10-30 23:07:34 +0100

    Coding Phase 5 test1  login

---
+++ test1.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 30 20:57:27 2020
+
+@author: ANANTHA KRISHNAN O M
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 30 13:13:58 2020
+
+@author: ANANTHA KRISHNAN O M
+"""
+
+import random as rn
+import csv
+
+
+def atm(bal):
+    restart=['Y','y','Yes','yes']
+    temp=restart
+    mon=[50,100,200,500,2000]
+    chance=1
+    tempbalance=bal
+    balance=0
+    
+    pn=0
+   
+    
+    while chance>0:
+        pin=input("\n Please Enter your PIN code: ")
+        with open("namepin.csv", 'r') as file:
+            file = csv.DictReader(file)
+            for acc in file:
+                if pin == acc['pin']:
+                   pn=acc['pin']
+                   print('\n \n Welcome ',acc['name'])
+        if pin == pn:
+            print("\n You pin looks cool ! \n")
+            while restart not in ('n', 'N', 'NO', 'no'):
+                print('Press 1 for BALANCE \n')
+                print('Press 2 for WITHDRAWAL\n')
+                print('Press 3 for PAY \n')
+                print('Press 4 for REMOVE THE CARD \n')
+                opt=int(input(" \n What would you like to choose: "))
+                
+                if opt ==1:
+                    print("\n Your BALANCE is: ", tempbalance)
+                    x=input(" \n Go Back ?: ")
+                    if not x in restart:
+                        print(" Thank You !")
+                        chance-=1
+                        break
+                elif opt == 2:
+                   
+                    print(" \n \n Please Choose Denominations of 50, 100, 200, 500, 2000")
+                    withdraw=float(input(" \n Enter the amount to withdraw: "))
+                    if withdraw in mon:
+                        balance=balance-withdraw
+                        print(" \n Your New  BALANCE is: ", balance)
+                    x=input(" \n Go Back ?: ")
+                    if  not x  in restart:
+                            print(" \n Thank You !")
+                            chance-=1
+                            break
+                    elif withdraw not in mon:
+                        print("\n Invalid amount !")
+                        restart=temp
+                elif opt ==3:
+                    py=float(input(("\n  How Much to PAY: ")))
+                    balance+=py
+                    print(" \n Your BALANCE is: ", balance)
+                    x=input(" \n Go Back ?: ")
+                    if x not in restart:
+                        print(" \n Thank You !")
+                        chance-=1
+                        break
+                    
+                elif opt==4:
+                    print("\n Your Card is Removed \n Thank You !")
+                    
+                    restart!=temp
+                    chance-=1
+                    break
+                else:
+                    print("\n Choose Correct Option  !")
+                    restart=temp
+        elif pin != pn:
+            print("\n Incorrect Password !")
+            chance-=1
+            if chance==0:
+                print("\n No more tries  !")
+                break
+    
+bal=int(rn.randrange(5000,100000))
+
+
+print(" \n \n Welcome to POPCORN ATM")
+atm(bal)
+
